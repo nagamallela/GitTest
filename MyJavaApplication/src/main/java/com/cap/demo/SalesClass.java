02a2095342fdaeabd0d1502b913055cbd67820eb
nmallela <nmallela@DIN76000180.corp.capgemini.com>
2017-04-05 19:17:50 +0530

    Loop Added

--- MyJavaApplication/src/main/java/com/cap/demo/SalesClass.java
+++ MyJavaApplication/src/main/java/com/cap/demo/SalesClass.java
@@ -2,10 +2,14 @@ package com.cap.demo;
 
 public class SalesClass {
 	
-	private int[] datas={1,90,45,-34};
+	private int[] datas={1,90,45,-34,12};
 	
 	public void display(){
 		System.out.println("Sales Class Display Method");
+		for(int i=0;i<datas.length;i++)
+		{
+			System.out.println(""+datas[i]);
+		}
 	}
 
 }
